939108675aa2d4aa4822e400c19ede77d8aadfaf
teacheryonate <teacheryo@nate.com>
2023-04-07 23:37:26 +0900

    최초 푸시

---
+++ xgeneration/src/main/java/com/example/demo/XgenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XgenerationApplication.class, args);
+	}
+
+}
