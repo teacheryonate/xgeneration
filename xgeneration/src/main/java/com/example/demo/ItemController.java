c9401d9c1502e3d3a3ff4101fef37e6785f1ec48
teacheryonate <teacheryo@nate.com>
2023-04-08 09:54:40 +0900

    1. application.properties에 데이터 입력 2. application-prod.properties 추가 생성 및 데이터 입력 3. ItemController에서 properties 파일에 입력한 데이터 사용하는 코드 추가 4. pom.xml에 버전 정보 업그레이드

--- xgeneration/src/main/java/com/example/demo/ItemController.java
+++ xgeneration/src/main/java/com/example/demo/ItemController.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/itme")
+@RequestMapping("/")
 public class ItemController {
+	
+	@Value("${hello.msg}")
+	private String msg;
 	
 	@GetMapping("")
 	public String say() {
-		return "안녕하세요";
+		return "안녕하세요, "+msg;
 	}
 
 }
